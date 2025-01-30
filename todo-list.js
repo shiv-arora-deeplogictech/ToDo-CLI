bfc8e481db777127acbb63fa684a54a051c27b21
Shiv Arora <shiv.arora@deeplogictech.com>
2025-01-30 16:17:12 +0530

    Added the console logs

--- todo-list.js
+++ todo-list.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const fs = require("fs");
 
 if(!fs.existsSync("todos.json")){
@@ -7,17 +9,20 @@ if(!fs.existsSync("todos.json")){
 const todos = JSON.parse(fs.readFileSync("todos.json"));
 
 
-const [command, input] = process.argv;
-console.log(process.argv);
+const [,, command, input] = process.argv;
+
 
 function getId(){
-    if(todos.length()===0){
+    if(todos.length===0){
         return 1; 
     } else {
         return todos[todos.length -1].id+1;
     }
 }
 
+function saveToDo(){
+    fs.writeFileSync("todos.json",JSON.stringify(todos));
+}
 switch(command){
 
     case "add":
@@ -26,14 +31,18 @@ switch(command){
             message: input
         }
         todos.push(newTodo);
+        saveToDo();
+        console.log("Added succesfully");
         break;
     case "view":
         for(let todo of todos){
-            console.table(todo);
+            console.table(todos);
         }
         break;
     case "delete":
         const index = todos.findIndex(todo=>todo.id === parseInt(input));
-        todos.slice(index,1);
+        todos.splice(index,1);
+        saveToDo();
+        console.log("Deleted succesfully");
         break;
 }
